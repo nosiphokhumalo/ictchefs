56e49dae493b9740b877937323ea5dd283d153dc
Nosipho Khumalo <nosiphokhumalo13@gmail.com>
2017-09-28 18:19:43 +0200

    Restricted access to logged-in users for all views

--- adminapp/urls.py
+++ adminapp/urls.py
@@ -1,12 +1,13 @@
 from django.conf.urls import url
+from django.contrib.auth.decorators import login_required
 from adminapp import views
 
 urlpatterns = [
     url(r'^$', views.HomePageView.as_view()),
-    url(r'^menu/$', views.MenuPageView.as_view()),
-    url(r'^add/$', views.AddPageView.as_view()),
-    url(r'^statistics/$', views.StatisticsPageView.as_view()),
-    url(r'^students/', views.StudentList.as_view(), name='students'),
+    url(r'^menu/$', login_required(login_url='/login/')(views.MenuPageView.as_view())),
+    url(r'^add/$', login_required(login_url='/login/')(views.AddPageView.as_view())),
+    url(r'^statistics/$', login_required(login_url='/login/')(views.StatisticsPageView.as_view())),
+    url(r'^students/', login_required(login_url='/login/')(views.StudentList.as_view()), name='students'),
     url(r'^filter/', views.filter, name='filter'),
     url(r'^login/', views.user_login, name='login'),
     url(r'^logout/$', views.user_logout, name='logout'),
