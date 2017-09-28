032ed7495bc23af42c6ebb0c0deff73ffd401404
Nosipho Khumalo <nosiphokhumalo13@gmail.com>
2017-09-28 17:39:13 +0200

    Added login functionality

--- adminapp/urls.py
+++ adminapp/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     url(r'^statistics/$', views.StatisticsPageView.as_view()),
     url(r'^students/', views.StudentList.as_view(), name='students'),
     url(r'^filter/', views.filter, name='filter'),
+    url(r'^login/', views.user_login, name='login'),
+    url(r'^logout/$', views.user_logout, name='logout'),
 ]
--- adminapp/views.py
+++ adminapp/views.py
@@ -1,6 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import TemplateView
 from django.db.models import Max
+
+from django.contrib.auth import authenticate, login, logout
+from django.http import HttpResponseRedirect, HttpResponse
+from django.contrib.auth.decorators import login_required
+from django.template import RequestContext
 
 from .models import Student, StudentInfo
 from django.http import Http404
@@ -16,6 +21,35 @@ class HomePageView(TemplateView):
     def get(self, request, **kwargs):
         return render(request, 'index.html', context=None)
 
+def user_login(request):
+    context = RequestContext(request)
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(username=username, password=password)
+        if user:
+            if user.is_active:
+                login(request, user)
+                return HttpResponseRedirect('/menu/')
+            else:
+                return HttpResponse("Your account is disabled.")
+        else:
+            return HttpResponse("Invalid login details supplied. Please try again.")
+
+    # The request is not a HTTP POST, so display the login form.
+    else:
+        return render(request, 'index.html', context)
+
+# Use the login_required() decorator to ensure only those logged in can access the view.
+@login_required(login_url='/login/')
+def user_logout(request):
+    # Since we know the user is logged in, we can now just log them out.
+    logout(request)
+
+    # Take the user back to the homepage.
+    return HttpResponseRedirect('/')
+
+        
 class MenuPageView(TemplateView):
     template_name = "menu.html"
 
@@ -29,6 +63,7 @@ class AddPageView(TemplateView):
             raise Http404('StudentInfo does not exist')
         return render(request, self.template_name,
                       {'classes': classes})
+                      
 
 class StatisticsPageView(TemplateView):
 
@@ -49,7 +84,7 @@ class StatisticsPageView(TemplateView):
         return render(request, self.template_name,
                       {'all_students': all_students, 'deceased': deceased, 'graduates': graduates, 'dropouts': dropouts, 'employed': employed, 'unemployed': unemployed, 'classes': classes})
 
-			
+@login_required(login_url='/login/')			
 def filter(request):
     yearstart = request.POST.get('yearstart') 
     yearend = request.POST.get('yearend')
